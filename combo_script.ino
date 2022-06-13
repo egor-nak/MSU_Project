a7a499286ccf777d9af6ab509623babe504e958a
Egor Nackonechnyi <67370877+egor-nak@users.noreply.github.com>
2022-06-13 17:56:00 +0300

    Add files via upload

--- combo_script.ino
+++ combo_script.ino
@@ -1,113 +1,137 @@
+// Include library
+#include <TMC2208Stepper.h>
+#include <Servo.h>
+#include <GyverHX711.h>
+#include <ArduinoSTL.h>
+
+
+
+// Помечаем нужные пины
 #define EN_PIN    38  // LOW: Driver enabled. HIGH: Driver disabled
 #define STEP_PIN  54  // Step on rising edge
 #define DIR_PIN 55
 #define X_MIN 2
 
-#include <TMC2208Stepper.h> // Include library
-#include <Servo.h>
-TMC2208Stepper driver = TMC2208Stepper(&Serial1);  // Create driver and use
-                                                 // HardwareSerial0 for communication
+bool button_was_pushed = false;
+bool direct = false; // true - наверх, false - вниз
+
+
+// обозначаем используемые элементы
+TMC2208Stepper driver = TMC2208Stepper(&Serial1);
 Servo myservo;
+GyverHX711 sensor(21, 20, HX_GAIN32_B);
+// HX_GAIN128_A - канал А усиление 128
+// HX_GAIN32_B - канал B усиление 32
+// HX_GAIN64_A - канал А усиление 64
 
-uint32_t last_time = 0;
-bool dir = true;
-double pos = 0;
-bool go_down = true;
-int counter = 0;
-
+void go_down_until_end();
 
 void setup() {
+
+  // Обозначаем порты вывода
   Serial.begin(250000);
-  Serial.println("Start...");
-  Serial1.begin(250000);        // Start hardware serial 1
-  driver.push();                // Reset registers
+  Serial1.begin(250000);
+  
+  driver.push();
 
   // Prepare pins
   pinMode(EN_PIN, OUTPUT);
   pinMode(STEP_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
   pinMode(X_MIN, OUTPUT);
-  digitalWrite(EN_PIN, HIGH);   // Disable driver in hardware
 
+  
+  // Настройка шаговика
+  digitalWrite(EN_PIN, HIGH);   // Disable driver in hardware
   driver.pdn_disable(true);     // Use PDN/UART pin for communication
   driver.I_scale_analog(false); // Use internal voltage reference
   driver.rms_current(500);      // Set driver current 500mA
   driver.toff(2);               // Enable driver in software
-
   digitalWrite(EN_PIN, LOW);    // Enable driver in hardware
   digitalWrite(DIR_PIN, LOW); // Если HIGH - то едет вниз, а если LOW - то вверх
+  direct = true;
 
   uint32_t data = 0;
-  Serial.print("DRV_STATUS = 0x");
   driver.DRV_STATUS(&data);
-  Serial.println(data, HEX);
+
+
+  // Настройка сервы
   myservo.attach(11);
+
+
+  // Настройка концевика
   attachInterrupt(0, myEventListener, CHANGE);
+
+
+  // Настройка АЦП
+  sensor.tare();
 }
+
 
 void loop() {
-  if (counter == 0) {
-    digitalWrite(DIR_PIN, HIGH);
-    dir = false;
+  
+  for (int pos = 0; pos <= 180; pos += 3.6) { //серва куртится по часовой
+     myservo.write(pos);
+     delay(1000);
+     go_up_until_end();
+     go_down_until_end();
+  }
+
+  for (int pos = 180; pos >= 0; pos -= 3.6) { //серва куртится против часовой
+     myservo.write(pos);
+     delay(1000);
+     go_up_until_end();
+     go_down_until_end();
+  }
+    
+}
+
+void go_down_until_end() {
+  
+  digitalWrite(DIR_PIN, HIGH); // он теперь едет вниз
+  direct = false;
+  while (!button_was_pushed) { // едем вниз пока не нажмем на концевик
     digitalWrite(STEP_PIN, HIGH);
-    delay(1);                     // Мотор работает только когда мы включаем и выключаем пины
+    delay(1);
     digitalWrite(STEP_PIN, LOW);
     delay(1);
-    Serial.println("DOWN");
-  } else {
-    Serial.println("NNNNNNNDOWN");
-    digitalWrite(DIR_PIN, LOW);
-    dir = true;
-    run_fc();
   }
+
+  digitalWrite(DIR_PIN, LOW); // он теперь едет вверх
+  direct = true;
+  button_was_pushed = false;
+  for (int i = 0; i < 100; ++i) {
+    digitalWrite(STEP_PIN, HIGH);
+    delay(1);
+    digitalWrite(STEP_PIN, LOW);
+    delay(1);
+  }
+
+  digitalWrite(DIR_PIN, HIGH); // он теперь едет вниз
+  direct = false;
 }
 
-void run_fc() {
-  for (pos = 0; pos <= 180.0; pos += 3.6) {
-    myservo.write(pos);
-    delay(1000);
-    for (int i = 0; i < 2; ++i) {
-      uint32_t ms = millis(); // выдаёт время в милисекундах с момента начала работы программы
-      while ((millis() - ms) <= 14000) { // пока не пройдет четырнадцать секунд
-        digitalWrite(STEP_PIN, HIGH);
-        delay(1);                     // Мотор работает только когда мы включаем и выключаем пины
-        digitalWrite(STEP_PIN, LOW);
-        delay(1);
-      }
-      if (dir) {    // Меняем направление движения
-        digitalWrite(DIR_PIN, HIGH);
-        dir = false;
-      } else {
-        digitalWrite(DIR_PIN, LOW);
-        dir = true;
-      }
-    }
+
+void go_up_until_end() {
+  
+  digitalWrite(DIR_PIN, LOW); // он теперь едет вверх
+  direct = true;
+  
+  int prev = 0;
+  if (sensor.available()) {
+      prev = sensor.read();
   }
-  for (pos = 180; pos >= 0.0; pos -= 3.6) {
-    myservo.write(pos);
-    delay(1000);
-    for (int i = 0; i < 2; ++i) {
-      uint32_t ms = millis();
-      while ((millis() - ms) <= 14000) {
-        digitalWrite(STEP_PIN, HIGH);
-        delay(1);
-        digitalWrite(STEP_PIN, LOW);
-        delay(1);
-      }
-      if (dir) {
-        digitalWrite(DIR_PIN, HIGH);
-        dir = false;
-      } else {
-        digitalWrite(DIR_PIN, LOW);
-        dir = true;
-      }
-    }
+  Serial.println(sensor.read());
+  while (sensor.read() <= 800000) { // едем вверх пока не изменится давление
+    digitalWrite(STEP_PIN, HIGH);
+    delay(1);
+    digitalWrite(STEP_PIN, LOW);
+    delay(1);
+    prev = sensor.read();
+    Serial.println(sensor.read());
   }
 }
-
 
 void myEventListener() {
-  go_down = false;
-  counter++;
-  Serial.println("Done");
+  button_was_pushed = true;
 }
